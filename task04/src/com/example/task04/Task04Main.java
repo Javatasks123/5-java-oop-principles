26f41d8a9715bc11308742fc475a17fcd0b4288a
Я <artem-shafikov@inbox.ru>
2025-10-19 19:30:53 +0500

    шафиков модуль 5

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,8 @@
 package com.example.task04;
 
-public class Task04Main {
-    public static void main(String[] args) {
-
+public class Task04Main
+{
+    public static void main(String[] args)
+    {
     }
 }
